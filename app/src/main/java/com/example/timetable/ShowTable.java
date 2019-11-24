e96af3838063c6c22185a1fdded7c3b6f174de73
Himanshu <gangravi5@gmail.com>
2019-11-24 22:58:09 +0530

    Second Commit

--- app/src/main/java/com/example/timetable/ShowTable.java
+++ app/src/main/java/com/example/timetable/ShowTable.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -11,61 +12,55 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class ShowTable extends AppCompatActivity {
-    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference mGetReference = mDatabase.getReference();
 
-
-
+    TextView t11;
+    TextView t12;
+    TextView t13;
+    TextView t14;
+    TextView t15;
+    TextView t16;
+    TextView t17;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_table);
+        ArrayList<Object> blockid = new ArrayList<Object>();
+        t11 = findViewById(R.id.lecture11);
+        t12 = findViewById(R.id.lecture12);
+        t13 = findViewById(R.id.lecture13);
+        t14 = findViewById(R.id.lecture14);
+        t15 = findViewById(R.id.lecture15);
+        t16 = findViewById(R.id.lecture16);
+        t17 = findViewById(R.id.lecture17);
 
-        mGetReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        blockid.add(t11);
+        blockid.add(t12);
+        blockid.add(t13);
+        blockid.add(t14);
+        blockid.add(t15);
+        blockid.add(t16);
+        blockid.add(t17);
 
-                if (dataSnapshot.exists()){
-                    HashMap<String, Object> dataMap = (HashMap<String, Object>) dataSnapshot.getValue();
+        Random rand =new Random();
+        //System.out.println(blockid.get(rand.nextInt(blockid.size())));
+        TextView obj = (TextView) blockid.get(rand.nextInt(blockid.size()));
+        blockid.remove(obj);
 
-                    for (String key : dataMap.keySet()){
+        ArrayList<String> faculty = new ArrayList<String>();
+        faculty.add("CC(AK)");
+        faculty.add("DS(AD)");
+        faculty.add("SC(SC)");
+        faculty.add("AI(JP)");
+        faculty.add("HV(S)");
 
-                        Object data = dataMap.get(key);
-
-                        try{
-                            HashMap<String, Object> userData = (HashMap<String, Object>) data;
-
-                            User mUser = new User((String) userData.get("name"), (int) (long) userData.get("age"));
-                            addTextToView(mUser.getName() + " - " + Integer.toString(mUser.getAge()));
-
-                        }catch (ClassCastException cce){
-
-// If the object can’t be casted into HashMap, it means that it is of type String.
-
-                            try{
-
-                                String mString = String.valueOf(dataMap.get(key));
-                                addTextToView(mString);
-
-                            }catch (ClassCastException cce2){
-
-                            }
-                        }
-
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
+        //String s = faculty.get(rand.faculty);
+        obj.setText("DS");
 
     }
 }
