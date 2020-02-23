9a594df4e08638f707501c2f6480a258439f2c74
Himanshu <gangravi5@gmail.com>
2020-02-23 23:10:35 +0530

    Home Page Button Created

--- app/src/main/java/com/example/timetable/MainActivity.java
+++ app/src/main/java/com/example/timetable/MainActivity.java
@@ -8,9 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-
-
+    Button button,homePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +25,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        homePage=findViewById(R.id.homepage);
+        homePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent i = new Intent(MainActivity.this,HomePage.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
 
 }
