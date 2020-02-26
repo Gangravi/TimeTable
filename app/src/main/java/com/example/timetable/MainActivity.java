0fc7a1c1ad1b072a1ba3375b7ee4733bb6ca2417
Himanshu <gangravi5@gmail.com>
2020-02-26 17:03:37 +0530

    Firebase code added as asynctask in main activity

--- app/src/main/java/com/example/timetable/MainActivity.java
+++ app/src/main/java/com/example/timetable/MainActivity.java
@@ -2,19 +2,33 @@ package com.example.timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
     Button button,homePage;
+    DatabaseReference rootRef,demoRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button);
+
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        demoRef = rootRef.child("app");
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +50,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        private String value;
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            demoRef.child("app").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    value = dataSnapshot.getValue(String.class);
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+            return value;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            Log.d("himanshu",result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/timetable/ShowDatabase.java
+++ app/src/main/java/com/example/timetable/ShowDatabase.java
@@ -3,12 +3,43 @@ package com.example.timetable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ShowDatabase extends AppCompatActivity {
+public class ShowDatabase extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinner;
+    private static final String[] semesters = {"semester 1", "semester 2", "semester 3","semester 4", "semester 5", "semester 6","semester 7", "semester 8"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_database);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(ShowDatabase.this,android.R.layout.simple_spinner_item,semesters);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Selected User: "+semesters[position] ,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
+    }
+
+
 }
+
+
